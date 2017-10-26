f745db056e7c01ba74ce837897ec8a2dca457f9e
zj0715zh <1372780005@qq.com>
2017-10-26 21:44:47 +0800

    Signed-off-by: zj0715zh <1372780005@qq.com>

---
+++ src/module/logcms/App.vue
@@ -0,0 +1,12 @@
+<template>
+	<div id="app">
+		<h1>这是后台网络日志录入页</h1>
+	</div>
+</template>
+<style lang="css" src="./logcms.css">
+</style>
+<script type="text/javascript">
+	export default {
+		name:'logcms'
+	}
+</script>
---
+++ src/module/logcms/logcms.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import axios from 'axios'
+import promise from 'es6-promise'
+import App from './App'
+import router from 'src/router/logcms'
+import store from 'src/store'
+import * as filters from 'src/filters'
+import 'common/css/reset.css'
+import 'common/css/iconfont/iconfont.css'
+const FastClick = require('fastclick')
+Vue.prototype.$ajax = axios
+promise.polyfill()
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
+FastClick.attach(document.body)//取消click事件的延迟
+var logcms,app;
+logcms = {
+	init(){
+		app = new Vue({
+			el: '#app',
+			template: '<App/>',
+			components: { App }
+		});
+	}
+}
+logcms.init();
