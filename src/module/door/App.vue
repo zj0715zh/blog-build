0e8928eb941bc5647a4855504f501ca63bd37b0b
itKingOne <1372780005@qq.com>
2017-11-08 14:35:16 +0800

    Signed-off-by: itKingOne <1372780005@qq.com>

--- src/module/door/App.vue
+++ src/module/door/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <video id="bgvid" autoplay="" loop="" muted="" poster="//static.zhuwenlong.com/image/index/cover-820e030cca.jpg">
+      <source src="//static.zhuwenlong.com/video/bgvideo-0c73e2c57a.mp4" type="video/mp4">
+      <source src="//static.zhuwenlong.com/video/bgvideo-513397179e.webm" type="video/webm">
+      <source src="//static.zhuwenlong.com/video/bgvideo-5428b1617d.ogv" type="video/ogg">
+    </video>
     <section class="head">
       <div class="head_main">
         <ul class="nav left">
